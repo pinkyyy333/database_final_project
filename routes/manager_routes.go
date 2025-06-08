62fccb0fd609d02ecdef12a3f9064f953cd168c8
Lin,Pei-Yu <crystal.sdlin.mg11@nycu.edu.tw>
2025-06-09 01:45:35 +0800

    更改login v2

--- routes/manager_routes.go
+++ routes/manager_routes.go
@@ -9,6 +9,7 @@ import (
 func RegisterManagerRoutes(r *gin.Engine) {
 	m := r.Group("/api/v1/manager")
 	{
+		m.POST("/login", controllers.LoginManager)
 		m.POST("/doctors", controllers.CreateDoctor)
 		m.GET("/doctors", controllers.GetAllDoctors)
 		m.PUT("/doctors/:id", controllers.UpdateDoctor)

0feb9a6d79d6888fe36ca11be39076bd26fc717c
Lin,Pei-Yu <crystal.sdlin.mg11@nycu.edu.tw>
2025-06-09 19:56:02 +0800

    醫生管理員V1

--- models/manager.go
+++ models/manager.go
@@ -1,7 +1,15 @@
 package models
 
+import "time"
+
 type Manager struct {
-	ManagerID uint   `gorm:"primaryKey;column:manager_id" json:"manager_id"`
-	Username  string `json:"username"`
-	Password  string `json:"-"`
+	ManagerID uint      `gorm:"primaryKey;column:manager_id" json:"manager_id"`
+	Username  string    `gorm:"column:username" json:"username"`
+	Password  string    `gorm:"column:password" json:"-"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+}
+
+func (Manager) TableName() string {
+	return "managers"
 }
